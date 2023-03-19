0e1e5815507f2c6d38281841c7df3e4dd143cf15
Tomaž Hribernik <tomaz.hribernik@protonmail.com>
2023-03-19 23:06:45 +0100

    Semi working Bash completion

--- src/initialize.sh
+++ src/initialize.sh
@@ -6,6 +6,6 @@
 ## Feel free to empty (but not delete) this file.
 
 CURRENT_USER="$(whoami)"
-LOCAL_CACHE="/home/$CURRENT_USER/.local/share/simple_docker_run"
+LOCAL_CACHE="$HOME/.local/share/simple_docker_run"
 
 mkdir -p "$LOCAL_CACHE"
