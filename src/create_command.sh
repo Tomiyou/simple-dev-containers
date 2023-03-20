8df90beec369f48b8bf568173a9f775aa7e60e77
Iztok Blazinsek <iztok@plume.com>
2023-03-20 23:03:25 +0100

    Fixes and enhancements
    1. typos
    2. enhanced create command to import docker recipes
    3. added ls command
    4. fixed install/uninstall script
    5...
    
    Signed-off-by: Iztok Blazinsek <iztok@plume.com>

--- src/create_command.sh
+++ src/create_command.sh
@@ -1,12 +1,14 @@
 
 get_container_image_name "${args[container_name]}"
 SOURCE_IMAGE="${args[source_image]}"
+DOCKER_RECIPE="${args[docker_recipe]}"
 
 # Create temporary Dockerfile
 TMP_DOCKERFILE="/tmp/simple_docker_run_$(date +%s)"
 touch "$TMP_DOCKERFILE"
 
 cat > "$TMP_DOCKERFILE" <<- EOF
+
 FROM $SOURCE_IMAGE
 
 # Label tells us which containers were created by this script
@@ -16,6 +18,13 @@ RUN apt-get update
 RUN apt-get install -y git fakeroot build-essential ncurses-dev \\
     xz-utils libssl-dev bc flex libelf-dev bison gcc llvm clang curl \\
     wget vim
+
+EOF
+
+# append docker recipe to the general recipe we have above
+[ -e "$DOCKER_RECIPE" ] && cat "$DOCKER_RECIPE" >> "$TMP_DOCKERFILE"
+
+cat >> "$TMP_DOCKERFILE" <<- EOF
 
 RUN useradd --create-home --shell /bin/bash $CURRENT_USER
 USER $CURRENT_USER
--- src/uninstall_command.sh
+++ src/uninstall_command.sh
@@ -1,6 +1,11 @@
 
 # Script will remove itself
+SCRIPT_NAME="simple-docker-run"
+SCRIPT_PATH="$(pwd)/$SCRIPT_NAME"
+SCRIPT_SYMLINK="$HOME/.local/bin/$SCRIPT_NAME"
 
-sed -i '/simple-docker-run completions/d' "$HOME/.bashrc"
-
+if [ -e "$SCRIPT_SYMLINK" ]; then
+	rm $SCRIPT_SYMLINK
+	sed -i '/simple-docker-run completions/d' "$HOME/.bashrc"
+fi
 trap script_uninstall_itself EXIT
