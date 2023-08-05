e40caff154aa25bbceb13a058ee4c2cfd42ecb4c
Tomaž Hribernik <tomaz.hribernik@protonmail.com>
2023-08-05 19:02:39 +0200

    Revert "Fixes and enhancements"
    This reverts commit 6ee6e89e3a7745efa10c43a7cce7a7c05afef661.
    
    Reverted code will be put into related repository.

--- src/list_command.sh
+++
@@ -1,2 +0,0 @@
-echo "Avaliable containers:"
-docker container ls
--- src/uninstall_command.sh
+++ src/uninstall_command.sh
@@ -1,11 +1,6 @@
 
 # Script will remove itself
-SCRIPT_NAME="simple-docker-run"
-SCRIPT_PATH="$(pwd)/$SCRIPT_NAME"
-SCRIPT_SYMLINK="$HOME/.local/bin/$SCRIPT_NAME"
 
-if [ -e "$SCRIPT_SYMLINK" ]; then
-	rm $SCRIPT_SYMLINK
-	sed -i '/simple-docker-run completions/d' "$HOME/.bashrc"
-fi
+sed -i '/simple-docker-run completions/d' "$HOME/.bashrc"
+
 trap script_uninstall_itself EXIT
